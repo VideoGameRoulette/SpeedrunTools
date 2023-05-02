802d4e24ebca54bba831af77c8b35f1322b7b893
Christopher D Couture <c.couture@exsi.ca>
2023-05-02 06:41:38 -0400

    HealthBar update

--- components/HealthBar/index.js
+++ components/HealthBar/index.js
@@ -7,7 +7,7 @@ const HealthBar = ({ id, current, max, percent, label, colors }) => {
     const pStr = `${p}%`;
     const sb = isDebug ? `[#${id}] ${label} ${current} / ${max}` : `${label} ${current} / ${max}`
     return (
-        <div className={classNames(max === 0 ? "hidden" : "", "relative border-2 h-8")}>
+        <div className={classNames(max === 0 || max > 100000 ? "hidden" : "", "relative border-2 h-8")}>
             <div className={classNames(colors[0], "w-full h-7")} style={{ width: pStr }}>
                 <div className="absolute w-full flex justify-between items-center text-xl px-2 font-bold">
                     <div className="text-white">
