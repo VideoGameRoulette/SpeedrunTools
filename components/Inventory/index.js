09f01dbe000300392cf36870f34c4d9634a94bcb
Christopher D Couture <c.couture@exsi.ca>
2023-05-11 23:42:27 -0400

    fixed inventory spacing when row is not equal to 4

--- components/Inventory/index.js
+++ components/Inventory/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { re2rItemImage, re2rWeaponImage } from './re2r';
 import { re3rItemImage, re3rWeaponImage } from './re3r';
+import { InventorySlot } from "./slot";
 
 const getItem = (item, disableBackground, isFatSlot, cbItem, cbWeapon, mainSlot, subSlot = null, shortcuts) => {
     if (isFatSlot) return <div className="hidden"></div>;
@@ -8,12 +9,17 @@ const getItem = (item, disableBackground, isFatSlot, cbItem, cbWeapon, mainSlot,
     return cbWeapon(item, disableBackground, mainSlot, subSlot, shortcuts);
 }
 
+const isInventorySlot = (idx, inventoryCount, item, disableBackground, isFatSlot, cbItem, cbWeapon, mainSlot, subSlot, shortcuts) => {
+    if (idx < inventoryCount) return getItem(item, disableBackground, isFatSlot, cbItem, cbWeapon, mainSlot, subSlot, shortcuts);
+    return <InventorySlot item={item} image="" disableBackground={true} mainSlot={mainSlot} subSlot={subSlot} shortcuts={shortcuts} />;
+}
+
 export const RE2RInventory = ({ items, inventoryCount, mainSlot, subSlot, shortcuts }) => {
     const [disableBackground, setBackground] = useState(false);
     return (
         <div className="w-[344px] flex flex-wrap justify-between" onClick={() => setBackground(prev => !prev)}>
             {items.map((item, idx) => (
-                Boolean(idx < inventoryCount)) && (getItem(item, disableBackground, idx > 0 ? items[idx - 1].IsFatSlot : false, re2rItemImage, re2rWeaponImage, mainSlot, subSlot, shortcuts))
+                isInventorySlot(idx, inventoryCount, item, disableBackground, idx > 0 ? items[idx - 1].IsFatSlot : false, re2rItemImage, re2rWeaponImage, mainSlot, subSlot, shortcuts))
             )}
         </div>
     )
@@ -24,7 +30,7 @@ export const RE3RInventory = ({ items, inventoryCount, mainSlot, subSlot, shortc
     return (
         <div className="w-[344px] flex flex-wrap justify-between" onClick={() => setBackground(prev => !prev)}>
             {items.map((item, idx) => (
-                Boolean(idx < inventoryCount)) && (getItem(item, disableBackground, idx > 0 ? items[idx - 1].IsFatSlot : false, re3rItemImage, re3rWeaponImage, mainSlot, subSlot, shortcuts))
+                isInventorySlot(idx, inventoryCount, item, disableBackground, idx > 0 ? items[idx - 1].IsFatSlot : false, re3rItemImage, re3rWeaponImage, mainSlot, subSlot, shortcuts))
             )}
         </div>
     )
